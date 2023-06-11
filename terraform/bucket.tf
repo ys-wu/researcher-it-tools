7419b39d2b15f0f742fbead968266a95c9a8f1ef
Yusheng Wu <yusheng@Yushengs-MacBook-Pro-2.local>
2023-06-11 18:23:11 +0800

    add additional objects to S3 bucket

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -2,13 +2,37 @@ resource "aws_s3_bucket" "b" {
   bucket = "researcher-it-tools.yushengwu.com"
 }
 
-resource "aws_s3_object" "object" {
+locals {
+  mime_types = jsondecode(file("mime.json"))
+}
+
+resource "aws_s3_object" "index" {
   depends_on = [aws_s3_bucket.b]
 
   bucket       = aws_s3_bucket.b.id
   key          = "index.html"
   source       = "../reveal.js/index.html" 
-  content_type = "text/html"
+  content_type = lookup(local.mime_types, ".html", null)
+}
+
+resource "aws_s3_object" "dist_object" {
+  depends_on = [aws_s3_bucket.b]
+
+  for_each     = fileset("../reveal.js/dist/", "**/*")
+  bucket       = aws_s3_bucket.b.id
+  key          = "dist/${each.value}"
+  source       = "../reveal.js/dist/${each.value}"
+  content_type = lookup(local.mime_types, can(regex("\\.[^.]+$", each.value)) ? regex("\\.[^.]+$", each.value) : "", null)
+}
+
+resource "aws_s3_object" "plugin_object" {
+  depends_on = [aws_s3_bucket.b]
+
+  for_each     = fileset("../reveal.js/plugin/", "**/*.js")
+  bucket       = aws_s3_bucket.b.id
+  key          = "plugin/${each.value}"
+  source       = "../reveal.js/plugin/${each.value}"
+  content_type = lookup(local.mime_types, can(regex("\\.[^.]+$", each.value)) ? regex("\\.[^.]+$", each.value) : "", null)
 }
 
 resource "aws_s3_bucket_public_access_block" "b_block" {
@@ -23,7 +47,7 @@ resource "aws_s3_bucket_public_access_block" "b_block" {
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
-  depends_on = [aws_s3_object.object]
+  depends_on = [aws_s3_object.index]
 
   bucket = aws_s3_bucket.b.id
 
